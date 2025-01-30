c613e87b9f2624ead718494c1f21f320b7923495
Damien <d.duval691@gmail.com>
2025-01-30 16:19:10 +0100

    Ajout des commentaires Paul & Damien

--- init/init.php
+++ init/init.php
@@ -1,6 +1,14 @@
 <?php
+    /*
+    * Fichier qui permet de déclarer la base de données, la connecter et y accéder depuis n'importe quel fichier
+    */
     require_once 'class/database.php';
-    $db = new database('localhost', '', 'root','maDataBase');
+
+    $db = new database(
+        'localhost', 
+        '', 
+        'root',
+        'maDataBase');
 
     $db->connect();
 ?>
